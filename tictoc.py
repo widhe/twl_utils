b3e02708127635977a9649fcdc7738755cabe21b
widhe <123471818+widhe@users.noreply.github.com>
2023-04-25 17:26:11 +0200

    Added flag to return string instead of printing to stdout

--- tictoc.py
+++ tictoc.py
@@ -1,19 +1,22 @@
 from datetime import datetime
-from datetime import timedelta
+
 
 class TicToc:
 
     def __init__(self):
         self.__start_time = datetime.now()
 
-    def toc(self):
+    def toc(self, sw_return_string=False):
         run_time = datetime.now() - self.__start_time
-        #run_time = datetime.now() + timedelta(seconds=390000) - self.__start_time
         days = run_time.days
         hours = run_time.seconds//3600
         mins = (run_time.seconds//60) % 60
         secs = run_time.seconds % 60
         if days > 0:
-            print(f"Execution time: {days} days {hours} hours {mins} minutes {secs} seconds")
+            str_message = f"Execution time: {days} days {hours} hours {mins} minutes {secs} seconds"
         else:
-            print(f"Execution time: {hours:02d}:{mins:02d}:{secs:02d}")
+            str_message = f"Execution time: {hours:02d}:{mins:02d}:{secs:02d}"
+        if sw_return_string:
+            return str_message
+        else:
+            print(str_message)
